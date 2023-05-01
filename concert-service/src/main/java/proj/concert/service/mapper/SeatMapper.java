516e4061376dfccf9590e853a5213b01963abbc5
uzji <else977@aucklanduni.ac.nz>
2023-05-01 19:28:40 +1200

    Added method for multiple mappings

--- concert-service/src/main/java/proj/concert/service/mapper/SeatMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/SeatMapper.java
@@ -3,8 +3,16 @@ package proj.concert.service.mapper;
 import proj.concert.common.dto.*;
 import proj.concert.service.domain.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class SeatMapper {
     public static SeatDTO mapSeat(Seat seat) {
         return new SeatDTO(seat.getLabel(), seat.getCost());
+    }
+
+    public static List<SeatDTO> mapSeats(List<Seat> seats) {
+        return seats.stream().map(e -> mapSeat(e)).collect(Collectors.toList());
     }
 }
