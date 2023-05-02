74a403ab29426af644051678b1476c4dca96a533
Kevin Wong <kevinxhwong@gmail.com>
2023-05-03 02:52:36 +1200

    removed unused imports in ConcertDTO

--- concert-common/src/main/java/proj/concert/common/dto/ConcertDTO.java
+++ concert-common/src/main/java/proj/concert/common/dto/ConcertDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
