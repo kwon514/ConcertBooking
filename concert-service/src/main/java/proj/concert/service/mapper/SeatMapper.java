502251fbd547a11c6797ba3d92d0a81e3b725aaa
Kevin Wong <kevinxhwong@gmail.com>
2023-05-03 02:53:42 +1200

    added Seat to SeatDTO method to seat mapper class

--- concert-service/src/main/java/proj/concert/service/mapper/SeatMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/SeatMapper.java
@@ -6,7 +6,6 @@ import proj.concert.service.domain.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public class SeatMapper {
     public static SeatDTO mapSeat(Seat seat) {
         return new SeatDTO(seat.getLabel(), seat.getCost());
@@ -14,5 +13,10 @@ public class SeatMapper {
 
     public static List<SeatDTO> mapSeats(List<Seat> seats) {
         return seats.stream().map(e -> mapSeat(e)).collect(Collectors.toList());
+    }
+
+    public static SeatDTO toSeatDTO(Seat seat) {
+        SeatDTO dto = new SeatDTO(seat.getLabel(), seat.getCost());
+        return dto;
     }
 }
