f625054515c4a6815f96ec7341e96ea6ad942331
uzji <else977@aucklanduni.ac.nz>
2023-05-05 16:24:30 +1200

    Added JPA versioning to Seat domain

--- concert-service/src/main/java/proj/concert/service/domain/Seat.java
+++ concert-service/src/main/java/proj/concert/service/domain/Seat.java
@@ -21,6 +21,9 @@ public class Seat {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 
+	@Version
+	private long version;
+
 	private String label;
 	private boolean isBooked;
 	private LocalDateTime date;
